0493db43050511b7f91e10ed05df94212fc651f4
= <=>
2024-01-05 17:25:09 -0600

    Cleaned up code
    - Cleaned up the Header class; only uses 1 <h1> and divs/spans
    - Cleaned up Button class hover colors

--- client/app/components/HeroSection.jsx
+++ client/app/components/HeroSection.jsx
@@ -15,29 +15,32 @@ const HeroSection = () => {
                 <div className="col-span-7 place-self-center">
 
                     {/* Header*/ }
-                    {/* Note: Changes must be applied to all 3 <h1>'s */}
-                    <h1 className="text-[#37444B] text-4xl md:text-5xl lg:text-6xl font-extrabold">
+                    <h1 className="text-[#37444B] mb-2 text-4xl md:text-5xl lg:text-6xl font-extrabold leading-[1.25] md:leading-[1.25] lg:leading-[1.25]">
                         Hello, I'm <span className="text-[#4A90E2]">John</span>,
+                        <br></br>
+                        and I love to
+                        <br></br>
+                        <div className="text-[#4A90E2] mt-1">
+                            <TypeAnimation
+                                sequence={[
+                                'Code.',
+                                1000,
+                                'Fence.',
+                                1000,
+                                'Learn.',
+                                1000,
+                                'Ball.',
+                                1000,
+                                'Work Out.',
+                                1000
+                                ]}
+                                wrapper="span"
+                                speed={10}
+                                repeat={Infinity}
+                            />
+                        </div>
                     </h1>
-                    <h1 className="text-[#37444B] mt-2 text-4xl md:text-5xl lg:text-6xl font-extrabold">and I love to</h1>
-                    <h1 className="text-[#4A90E2] mt-2 mb-4 text-4xl md:text-5xl lg:text-6xl font-extrabold">
-                        <TypeAnimation
-                            sequence={[
-                            
-                            'Code.',
-                            1000,
-                            'Fence.',
-                            1000,
-                            'Learn.',
-                            1000,
-                            'Ball.',
-                            1000
-                            ]}
-                            wrapper="span"
-                            speed={10}
-                            repeat={Infinity}
-                        />
-                    </h1>
+                    
                     
 
                     {/* SubHeader */}
@@ -46,12 +49,11 @@ const HeroSection = () => {
                     </p>
 
                     {/* Buttons */}
-                    {/* @TODO: change hover colors*/}
                     <div>
-                        <button className="px-12 py-3 w-full sm:w-fit rounded-xl mr-4 bg-[#4A90E2] hover:bg-[#37444B] text-white font-semibold">
+                        <button className="px-12 py-3 w-full sm:w-fit rounded-xl mr-4 bg-[#4A90E2] hover:bg-[#63a4ff] text-white font-semibold">
                             Blog
                         </button>
-                        <button className="px-10 py-3 w-full sm:w-fit rounded-xl  ml-1 bg-transparent hover:bg-slate-300 text-black border border-black mt-3 font-semibold">
+                        <button className="px-10 py-3 w-full sm:w-fit rounded-xl  ml-1 bg-transparent hover:bg-[#d0d0d0] text-black border border-black mt-3 font-semibold">
                             Contact Me
                         </button>
                     </div>
