3fb205dffa6bed3fd0e4b30c3a1951ba5152d0b6
= <=>
2024-01-05 17:05:20 -0600

    Added a typing animation using react-type-animation library

--- client/app/components/HeroSection.jsx
+++ client/app/components/HeroSection.jsx
@@ -1,6 +1,10 @@
+'use client'
+
 import React from 'react';
-import Image from 'next/image';
 import '../../styles/LandingPage.css';
+import { TypeAnimation } from 'react-type-animation';
+
+
 
 const HeroSection = () => {
     return (
@@ -10,12 +14,30 @@ const HeroSection = () => {
                 {/* First 7 columns (out of 12) */}
                 <div className="col-span-7 place-self-center">
 
-                    {/* Header */ }
-                    <h1 className="text-[#37444B] lg:text-6xl text-4xl font-extrabold">
+                    {/* Header*/ }
+                    {/* Note: Changes must be applied to all 3 <h1>'s */}
+                    <h1 className="text-[#37444B] text-4xl md:text-5xl lg:text-6xl font-extrabold">
                         Hello, I'm <span className="text-[#4A90E2]">John</span>,
                     </h1>
-                    <h1 className="text-[#37444B] mt-2 lg:text-6xl text-4xl font-extrabold">and I love to</h1>
-                    <h1 className="text-[#4A90E2] mt-2 mb-4 lg:text-6xl text-4xl font-extrabold">Code|</h1>
+                    <h1 className="text-[#37444B] mt-2 text-4xl md:text-5xl lg:text-6xl font-extrabold">and I love to</h1>
+                    <h1 className="text-[#4A90E2] mt-2 mb-4 text-4xl md:text-5xl lg:text-6xl font-extrabold">
+                        <TypeAnimation
+                            sequence={[
+                            
+                            'Code.',
+                            1000,
+                            'Fence.',
+                            1000,
+                            'Learn.',
+                            1000,
+                            'Ball.',
+                            1000
+                            ]}
+                            wrapper="span"
+                            speed={10}
+                            repeat={Infinity}
+                        />
+                    </h1>
                     
 
                     {/* SubHeader */}
@@ -26,10 +48,10 @@ const HeroSection = () => {
                     {/* Buttons */}
                     {/* @TODO: change hover colors*/}
                     <div>
-                        <button className="px-12 py-3 rounded-xl mr-4 bg-[#4A90E2] hover:bg-[#37444B] text-white font-semibold">
+                        <button className="px-12 py-3 w-full sm:w-fit rounded-xl mr-4 bg-[#4A90E2] hover:bg-[#37444B] text-white font-semibold">
                             Blog
                         </button>
-                        <button className="px-10 py-3 rounded-xl  ml-1 bg-transparent hover:bg-slate-300 text-black border border-black mt-3 font-semibold">
+                        <button className="px-10 py-3 w-full sm:w-fit rounded-xl  ml-1 bg-transparent hover:bg-slate-300 text-black border border-black mt-3 font-semibold">
                             Contact Me
                         </button>
                     </div>
@@ -37,7 +59,7 @@ const HeroSection = () => {
                 </div>
 
                 {/* Last 5 columns (out of 12) */}
-                <div className="col-span-5 place-self-center mt-4 lg:mt-0">
+                <div className="col-span-5 place-self-center mt-5 lg:mt-0 md:ml-5">
 
                     {/* Hero Image */}
                     <div className="w-[250px] h-[250px] sm:w-[325px] sm:h-[325px] md:w-[400px] md:h-[400px] relative">
--- client/app/page.js
+++ client/app/page.js
@@ -3,8 +3,10 @@ import HeroSection from './components/HeroSection';
 
 export default function Home(){
   return (
-    <main className="flex flex-col min-h-screen container mx-auto px-12 py-4">
-      <HeroSection />
+    <main className="flex min-h-screen flex-col bg-white">
+      <div className="container mx-auto px-12 py-4">
+        <HeroSection />
+      </div>
     </main>
 
   );
