167fc3a83afdb8be778ffddcf3fbb79416c0ee4b
= <=>
2024-01-10 14:11:07 -0600

    Finished "About" Section on Landing Page
    - Header
    - Paragraph underneath Header
    - Tabs with content
        - Tab Component

--- client/app/components/AboutSection.jsx
+++ client/app/components/AboutSection.jsx
@@ -4,6 +4,47 @@ import React, { useState, useTransition } from "react";
 import Image from "next/image";
 import TabButton from "./TabButton";
 
+// Contains information for every tab (content)
+// @TODO should be connected to database
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    // @TODO allow user to change in dashboard (DB)
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Lorem</li>
+        <li>Ipsum</li>
+        <li>Dolor</li>
+        <li>Sit</li>
+        <li>Amet</li>
+        <li>Consectetur</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Adipisicing, Delenti</li>
+        <li>Elit Eius, Sed</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Experience",
+    id: "experience",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Lorem Ipsum</li>
+        <li>Dolor Sit</li>
+        <li>Amet Consectetur</li>
+      </ul>
+    ),
+  },
+];
+
 const AboutSection = () => {
   // Hold information about which tab is open
   const [tab, setTab] = useState("skills");
@@ -23,10 +64,12 @@ const AboutSection = () => {
     <section className="text-black">
       <div className="md:grid md:grid-cols-2 gap-8 xl:gap-16 items-center py-8 px-4 sm:py-16 xl:px-16">
         {/* First Column (1/2) */}
-        <Image src="/images/about-me.png" width={500} height={500} />
+        <div className="flex flex-col h-full mt-4 md:mt-0">
+          <Image src="/images/about-me.png" width={500} height={500} />
+        </div>
 
         {/* Second Column (2/2) */}
-        <div className="font-sans">
+        <div className="flex flex-col h-full mt-4 md:mt-0 text-left font-sans">
           <p className="text-[#bcbaba] font-medium mb-2"> A LITTLE BIT </p>
           <h2 className="text-4xl font-mono font-bold text-[#37444B] mb-4">
             {" "}
@@ -35,7 +78,7 @@ const AboutSection = () => {
               About Me
             </span>{" "}
           </h2>
-          <p className="text-base lg:text-lg">
+          <p className="text-base lg:text-lg mt-3">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore. Lorem ipsum dolor sit
             amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
@@ -51,11 +94,23 @@ const AboutSection = () => {
               {" "}
               Skills
             </TabButton>
-            <span className="mr-3 font-semibold hover:text-black text-gray-200 border-b border-purple-500">
-              Skills
-            </span>
-            <span>Education</span>
-            <span>Experience</span>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              {" "}
+              Education
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("experience")}
+              active={tab === "experience"}
+            >
+              {" "}
+              Experience
+            </TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
           </div>
         </div>
       </div>
--- client/app/components/TabButton.jsx
+++ client/app/components/TabButton.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
 const TabButton = ({ active, selectTab, children }) => {
-    const buttonClasses = active ? 'text-black border-b border-purple-500' : 'text-[#ADB7BE]'
+    const buttonClasses = active ? 'text-[#37444b] border-b-2 border-[#4a90e2]' : 'text-[#ADB7BE]'
     return (
         <button onClick={selectTab}>
-            <p className={`mr-3 font-semibold ${buttonClasses}`}>
+            <p className={`mr-3 font-medium ${buttonClasses}`}>
                 {children}
             </p>
 
