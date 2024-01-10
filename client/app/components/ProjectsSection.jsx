8a66deef168fc19d435b3357ae08d67d233c82b2
= <=>
2024-01-10 17:14:40 -0600

    Finished Project Section (rough)

---
+++ client/app/components/ProjectsSection.jsx
@@ -0,0 +1,88 @@
+"use client";
+import React, { useState } from "react";
+import ProjectCard from "./ProjectCard";
+import ProjectTag from "./ProjectTag";
+
+const projectsData = [
+  {
+    id: 1,
+    title: "React Portfolio Website",
+    description: "Project 1 Description",
+    image: "/images/example.jpg",
+    tag: ["All", "Web"],
+    gitUrl: "/",
+    previewUrl: "/",
+  },
+  {
+    id: 2,
+    title: "React Portfolio Website",
+    description: "Project 2 Description",
+    image: "/images/example.jpg",
+    tag: ["All", "Web"],
+    gitUrl: "/",
+    previewUrl: "/",
+  },
+  {
+    id: 3,
+    title: "React Portfolio Website",
+    description: "Project 3 Description",
+    image: "/images/example.jpg",
+    tag: ["All", "Web"],
+    gitUrl: "/",
+    previewUrl: "/",
+  },
+];
+
+const ProjectSection = () => {
+  // track which filter is selected
+  const [tag, setTag] = useState("All");
+
+  // Updates tag
+  const handleTagChange = (newTag) => {
+    setTag(newTag);
+  };
+
+  // simply filter for projects
+  const filteredProjects = projectsData.filter((project) => 
+    project.tag.includes(tag)
+  );
+
+  return (
+    <div>
+      <h2 className="text-center text-4xl font-bold text-black mt-4 mb-7">
+        My Projects
+      </h2>
+      <div className="text-black flex flex-row justify-center items-center gap-2 py-6">
+        <ProjectTag
+          onClick={handleTagChange}
+          name="All"
+          isSelected={tag === "All"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="Web"
+          isSelected={tag === "Web"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="Mobile"
+          isSelected={tag === "Mobile"}
+        />
+      </div>
+      <div className="grid md:grid-cols-3 gap-8 md:gap-12">
+        {filteredProjects.map((project) => (
+          <ProjectCard
+            key={project.id}
+            title={project.title}
+            description={project.description}
+            imgUrl={project.image}
+            gitUrl={project.gitUrl}
+            previewUrl={project.previewUrl}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ProjectSection;
--- client/app/page.js
+++ client/app/page.js
@@ -6,6 +6,7 @@ import Image from 'next/image'
 import HeroSection from './components/HeroSection';
 import Navbar from './components/Navbar';
 import AboutSection from './components/AboutSection';
+import ProjectSection from './components/ProjectsSection';
 
 export default function Home(){
   return (
@@ -20,6 +21,8 @@ export default function Home(){
       <div className="container mt-24 mx-auto px-12 py-4">
         <HeroSection />
         <AboutSection />
+        <ProjectSection/>
+      
       </div>
 
 
