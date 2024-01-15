4463c0e6d7a031934fce372eb6d77de97e321eb6
= <=>
2024-01-15 17:01:26 -0600

    Additions to About Section

--- client/app/components/TabButton.jsx
+++ client/app/components/TabButton.jsx
@@ -16,7 +16,7 @@ const TabButton = ({ active, selectTab, children }) => {
       <motion.div
         animate={active ? "active" : "default"}
         variants={variants}
-        className="h-1 bg-[#4a90e2] mt-2 mr-3"
+        className="h-1 bg-[#4a90e2] mt-2 mr-3 rounded"
       ></motion.div>
     </button>
   );
